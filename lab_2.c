34e43c52dec542bdde61176394a8e36ee9b666b4
TokiharU <blagova_a@bk.ru>
2016-06-03 14:31:23 +0600

    finding the discriminant and the output to the screen

--- lab_2.c
+++ lab_2.c
@@ -7,6 +7,7 @@ int main ()
 	printf ("Enter the variable value: ");
 	scanf ("%f %f %f", &a, &b, &c);
 	printf ("(%.1f)x^2+(%.1f)x+(%.1f)=0", a, b, c);
-	
+	D=pow(b,2)-4*a*c;
+	printf("\nDiscriminant is %.1f", D);
 	return 0;
 }
