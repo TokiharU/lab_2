e871a63f5b3566fefb0b270a783bc615d8cbd290
TokiharU <blagova_a@bk.ru>
2016-06-03 14:35:27 +0600

    the presence of roots and output to the screen

--- lab_2.c
+++ lab_2.c
@@ -9,5 +9,22 @@ int main ()
 	printf ("(%.1f)x^2+(%.1f)x+(%.1f)=0", a, b, c);
 	D=pow(b,2)-4*a*c;
 	printf("\nDiscriminant is %.1f", D);
+	if (D > 0)
+	{
+		d=sqrt(D);
+		x1=(-b+d)/(2*a);
+		x2=(-b-d)/(2*a);
+		printf ("\nx1 = %.1f; x2 = %.1f", x1, x2);
+	}
+	if (D == 0)
+	{
+		x1=x2=-b/(2*a);
+		printf ("\nx1 = %.1f; x2 = %.1f", x1, x2);
+	}
+	if (D < 0)
+	{
+		printf ("\nno solutions");
+	}
+
 	return 0;
 }
