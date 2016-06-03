52f220d14f9c707a1a2e1e0a137a45e75a57410b
TokiharU <blagova_a@bk.ru>
2016-06-03 14:40:16 +0600

    merge

--- lab_2.c
+++ lab_2.c
@@ -4,11 +4,14 @@
 int main ()
 {
 	float a, b, c, D, d, x1, x2;
+	
 	printf ("Enter the variable value: ");
 	scanf ("%f %f %f", &a, &b, &c);
 	printf ("(%.1f)x^2+(%.1f)x+(%.1f)=0", a, b, c);
+	
 	D=pow(b,2)-4*a*c;
 	printf("\nDiscriminant is %.1f", D);
+	
 	if (D > 0)
 	{
 		d=sqrt(D);
