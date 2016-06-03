714c0d8e75256f11bee11e9e7645cdd421d4be84
TokiharU <blagova_a@bk.ru>
2016-06-03 14:29:35 +0600

    the input variables and the output equations

--- lab_2.c
+++ lab_2.c
@@ -4,6 +4,9 @@
 int main ()
 {
 	float a, b, c, D, d, x1, x2;
+	printf ("Enter the variable value: ");
+	scanf ("%f %f %f", &a, &b, &c);
+	printf ("(%.1f)x^2+(%.1f)x+(%.1f)=0", a, b, c);
 	
 	return 0;
 }
